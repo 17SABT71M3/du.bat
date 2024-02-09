762ccd30f77b892c3a7981d25f0b1d21f30cd842
Christian Programmer "Dr. Dream$ <117502862+175M3H3RE@users.noreply.github.com>
2024-02-09 23:17:52 +0530

    Add files via upload

---
+++ du_v4.4.bat
@@ -0,0 +1,15 @@
+@echo off
+set /a realtoken=0
+set filesize=
+if not exist %1 goto :eof
+set /a counter=0
+for /f %%i in ('dir /a-d /b %1') do set /a counter+=1
+if %counter% NEQ 1 goto :eof
+:loop
+set /a realtoken+=1
+for /f "tokens=%realtoken%" %%i in ('dir /a-d %1 2^>NUL^|findstr /r "^[0-9]"') do echo %%i|findstr /r "^[0-9]*$" >NUL&&set filesize=%%i
+if defined filesize goto trim
+goto :loop
+:trim
+set filesize=%filesize:,=%
+echo %filesize%
