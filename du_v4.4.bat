dc2407ba2e1e84352038d253e628994e7c60ee93
Christian Programmer "Dr. Dream$ <117502862+175M3H3RE@users.noreply.github.com>
2024-02-09 23:22:37 +0530

    Update du_v4.4.bat

--- du_v4.4.bat
+++ du_v4.4.bat
@@ -7,7 +7,7 @@ for /f %%i in ('dir /a-d /b %1') do set /a counter+=1
 if %counter% NEQ 1 goto :eof
 :loop
 set /a realtoken+=1
-for /f "tokens=%realtoken%" %%i in ('dir /a-d %1 2^>NUL^|findstr /r "^[0-9]"') do echo %%i|findstr /r "^[0-9]*$" >NUL&&set filesize=%%i
+for /f "tokens=%realtoken%" %%i in ('dir /a-d %1 2^>NUL^|findstr /r "^[0-9]"') do echo %%i|findstr /r "^[0-9,]*$" >NUL&&set filesize=%%i
 if defined filesize goto trim
 goto :loop
 :trim
